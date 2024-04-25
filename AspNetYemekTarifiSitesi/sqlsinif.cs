a81002940e285058c723e02184631f852c982617
Nihat Emir İleri <nightcorevanx26@gmail.com>
2024-04-25 19:17:04 +0300

    Yemeksitesi_38-50odev

---
+++ AspNetYemekTarifiSitesi/sqlsinif.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace AspNetYemekTarifiSitesi
+{
+    public class sqlsinif
+    {
+        public SqlConnection baglanti()
+        {
+            SqlConnection baglan = new SqlConnection(@"Data Source=emir\SQLEXPRESS;Initial Catalog=Dbo_yemektarifi;Integrated Security=True");
+            baglan.Open();
+            return baglan;
+        }
+    }
+}
